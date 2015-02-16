2eed193800455bdd3b6cd3aedf246fac7d9ae84c
Michael Marcel Longerich <mlongerich@gmail.com>
2015-02-16 15:40:07 +0700

    Adds Age and CMAC age to students#index

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,9 @@
+require 'date'
+
 module StudentsHelper
+
+  def age(dob, date)
+    date.year- dob.year - ((date.month > dob.month || (date.month == dob.month && date.day >= dob.day)) ? 0 : 1)
+  end
+
 end
