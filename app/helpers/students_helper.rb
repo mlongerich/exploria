014e6f8bdb584a248500e85ef2534984a4d12796
Michael Marcel Longerich <mlongerich@gmail.com>
2015-02-17 14:09:36 +0700

    Add grade 10, 11 and change end birthdate

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -3,7 +3,7 @@ require 'date'
 module StudentsHelper
 
   def age(dob, date)
-    date.year- dob.year - ((date.month > dob.month || (date.month == dob.month && date.day >= dob.day)) ? 0 : 1)
+    date.year - dob.year - ((date.month > dob.month || (date.month == dob.month && date.day >= dob.day)) ? 0 : 1)
   end
 
 end
